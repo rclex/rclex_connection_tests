99cef6dc3cbc1c6ca79b3da40504518dad1b67c8
kebus426 <kemu423@gmail.com>
2021-05-27 17:48:25 +0900

    fix test_in_docker.sh

--- test_in_docker.sh
+++ test_in_docker.sh
@@ -1,1 +1,9 @@
-sudo docker-compose exec rclex_docker bash -c 'cd rclex; MIX_ENV=test mix local.hex --force; MIX_ENV=test mix deps.get; MIX_ENV=test mix compile;  cd rclex_connection_tests; rm rclcpp/build/cpp_pubsub/CMakeCache.txt; source /opt/ros/dashing/setup.bash; ./entrypoint.sh'
+#!/bin/bash
+
+sudo docker-compose exec rclex_docker bash -c 'cd rclex && \ 
+    MIX_ENV=test mix deps.get && \ 
+    MIX_ENV=test mix compile && \ 
+    cd rclex_connection_tests && \ 
+    rm rclcpp/build/cpp_pubsub/CMakeCache.txt && \ 
+    source /opt/ros/dashing/setup.bash && \ 
+    ./entrypoint.sh'
