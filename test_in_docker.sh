4494f1e4427b8ef9476681b0dbd4a6280dc6cfb9
kebus426 <kemu423@gmail.com>
2021-05-27 17:13:44 +0900

    add docker-compose.yml to test

---
+++ test_in_docker.sh
@@ -0,0 +1,1 @@
+sudo docker-compose exec rclex_docker bash -c 'cd rclex; MIX_ENV=test mix local.hex --force; MIX_ENV=test mix deps.get; MIX_ENV=test mix compile;  cd rclex_connection_tests; rm rclcpp/build/cpp_pubsub/CMakeCache.txt; source /opt/ros/dashing/setup.bash; ./entrypoint.sh'
