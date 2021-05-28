6d87b9c2e7e227d2d9cb79f6dc2f7abaf3605797
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-05-28 17:52:39 +0900

    [fix #4] del unnessesary message when using docker env

--- test_in_docker.sh
+++ test_in_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo docker-compose exec rclex_docker bash -c 'cd /root/rclex_connection_tests && \
-    source /opt/ros/${ROS_DISTRO}/setup.bash && \ 
-    ./entrypoint.sh '
+sudo docker-compose exec rclex_docker \
+  bash -c 'cd /root/rclex_connection_tests &&
+  source /opt/ros/${ROS_DISTRO}/setup.bash &&
+  ./entrypoint.sh '
