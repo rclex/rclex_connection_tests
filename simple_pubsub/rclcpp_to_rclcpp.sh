0292e28020bc2b4b6ba03590e390acf5b3d053e3
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-07-14 17:28:15 +0900

    add rclcpp_to_rclcpp test

---
+++ simple_pubsub/rclcpp_to_rclcpp.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+cd $1
+
+ros2 run cpp_pubsub listener &
+PID1=$!
+
+echo "TESTINFO: wait for creating subscriber"
+while :
+do
+  if [ -e sub_ready.txt ]; then
+    echo "TESTINFO: subscriber has created"
+    rm -f sub_ready.txt
+    break
+  fi
+  sleep 0.1
+done
+
+ros2 run cpp_pubsub talker &
+PID2=$!
+
+wait $PID1 $PID2
+
+cppPub=`cat cpp_pub.txt | tr -d "\0"`
+echo "TESTINFO: published message  : $cppPub"
+rm -f cpp_pub.txt
+
+cppSub=`cat cpp_sub.txt | tr -d "\0"`
+echo "TESTINFO: subscribed message : $cppSub"
+rm -f ex_sub.txt
+
+test $cppPub = $cppSub
+result=$?
+echo "TESTINFO: result : $result"
+
+if [ $result -ne 0 ]; then
+    echo "TESTERROR: $0 failed."
+    exit 1
+fi
